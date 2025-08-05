24705ed2bba51cb193b3981841780c2864410467
danichagas <chagasdaniel788@gmail.com>
2025-08-05 14:56:38 -0300

    feat: create 'create' method

--- src/messages/repositories/messages.repository.ts
+++ src/messages/repositories/messages.repository.ts
@@ -15,6 +15,14 @@ export class MessageRepository {
     return messages
   }
 
-  async create(message: string) {
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8')
+    const messages = JSON.parse(contents)
+
+    const id = Math.floor((Math.random() * 999))
+
+    messages[id] = { id, content }
+
+    await writeFile('messages.json', JSON.stringify(messages))
   }
 }
