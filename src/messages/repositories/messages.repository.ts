141962da3d0620706df3266eb2a66b158c740556
danichagas <chagasdaniel788@gmail.com>
2025-08-05 14:39:39 -0300

    feat: create findOne method

---
+++ src/messages/repositories/messages.repository.ts
@@ -0,0 +1,18 @@
+import { readFile, writeFile } from 'fs/promises'
+
+export class MessageRepository {
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf-8')
+    const messages = JSON.parse(content)
+
+    return messages[id]
+  }
+
+  async findAll() {
+
+  }
+
+  async create(message: string) {
+
+  }
+}
